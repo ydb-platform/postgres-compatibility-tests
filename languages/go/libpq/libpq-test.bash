e445fb0f26af91935b29a61d6fe388d02345a816
Timofey Koolin <timofey.koolin@gmail.com>
2023-05-04 17:20:49 +0300

    --amend

--- languages/go/libpq/libpq-test.bash
+++ languages/go/libpq/libpq-test.bash
@@ -2,13 +2,21 @@
 
 set -eu
 
-DIR=$(dirname $0)
-cd "$DIR"
-wget https://github.com/lib/pq/archive/refs/tags/v1.10.9.tar.gz -O libpq.tar.gz
+[ -e ".tests-root-folder" ] || echo "Start from test root folder"
+[ -e ".tests-root-folder" ] || exit 1
 
-tar -zxvf libpq.tar.gz
+./scripts/install-go-test-converter.sh
 
-cd pq-1.10.9
+DIR="$PWD"
+
+TESTDIR="$DIR/tmp/go-libpq"
+rm -rf "$TESTDIR"
+mkdir -p "$TESTDIR/src"
+mkdir -p "$DIR/tmp/test-result"
+cd "$TESTDIR"
+
+wget https://github.com/lib/pq/archive/refs/tags/v1.10.9.tar.gz -O "$TESTDIR/libpq.tar.gz"
+tar --strip-components=1 -zxvf libpq.tar.gz -C src
 
 export PGUSER=root
 export PGHOST=localhost
@@ -17,4 +25,12 @@ export PQGOSSLTESTS=0
 export PQSSLCERTTEST_PATH=certs
 
 eval $(GIMME_GO_VERSION=1.20 gimme)
-PQTEST_BINARY_PARAMETERS=no go test -test.timeout=30s -v ./...
+cd src
+echo "Start test" >&2
+PQTEST_BINARY_PARAMETERS=no go test -json -test.timeout=30s -v > ../test-result.json || true
+
+cd "$DIR"
+echo "Convert result" >&2
+./tmp/bin/go-junit-report -parser gojson < "$TESTDIR/test-result.json" > "$DIR/tmp/test-result/go-libpq-1.xml"
+
+echo "Finished" >&2
