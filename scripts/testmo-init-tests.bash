aa246082ca52383af6e9c729165ede36c0877003
Timofey Koolin <timofey.koolin@gmail.com>
2023-05-23 12:57:29 +0300

    echo testmo source

--- scripts/testmo-init-tests.bash
+++ scripts/testmo-init-tests.bash
@@ -10,6 +10,8 @@ fi
 
 TESTMO_SOURCE="${TESTMO_SOURCE/_/-}"  # Replace _ to - for workflow_dispatch
 
+echo "Testmo source: '$TESTMO_SOURCE'"
+
 RUN_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
 
 testmo automation:resources:add-field --name git --type string --value ${GITHUB_SHA} --resources resources.json
