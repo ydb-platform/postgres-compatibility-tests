5812f0600ffb4903671cb96898dbaf610991c93a
Timofey Koolin <timofey.koolin@gmail.com>
2023-06-07 18:47:05 +0300

    build script process log out to stderr instead of stdout

--- scripts/run-script.bash
+++ scripts/run-script.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose -f scripts/report-processing/docker-compose.yaml run --build scripts "$@"
+docker-compose -f scripts/report-processing/docker-compose.yaml build >&2
+docker-compose -f scripts/report-processing/docker-compose.yaml run scripts "$@"
