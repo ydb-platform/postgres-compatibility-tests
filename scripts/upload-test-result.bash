18e05a31b9ed64bb7d3a46c2c6062ab3d3ed67e5
Timofey Koolin <timofey.koolin@gmail.com>
2023-06-09 14:12:24 +0300

    show report in github

--- scripts/upload-test-result.bash
+++ scripts/upload-test-result.bash
@@ -20,9 +20,21 @@ RUN_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
 testmo automation:resources:add-field --name git --type string --value ${GITHUB_SHA} --resources resources.json
 testmo automation:resources:add-link --name build --url $RUN_URL --resources resources.json
 
-testmo automation:run:submit \
+testmo automation:run:create \
     --instance "$TESTMO_URL" \
     --project-id "$TESTMO_PROJECT_ID" \
     --name "YDB Postgres compatibility" \
     --source "$TESTMO_SOURCE" \
-    --results "**/test-result/*.xml"
+    --resources resources.json \
+        > testmo-run-id.txt
+
+TESTMO_RUN_ID=$(cat testmo-run-id.txt)
+echo "Testmo report: https://nebius.testmo.net/automation/runs/view/$ID" >> $GITHUB_STEP_SUMMARY
+
+testmo automation:run:submit-thread \
+            --instance "$TESTMO_URL" \
+            --run-id "$TESTMO_RUN_ID" \
+            --results "**/test-result/*.xml"
+
+testmo automation:run:complete --instance "$TESTMO_URL" --run-id "$TESTMO_RUN_ID"
+
