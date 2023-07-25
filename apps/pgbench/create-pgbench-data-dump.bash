486111048d439eceabd9a99b6d14ccd689b1732f
Timofey Koolin <timofey.koolin@gmail.com>
2023-07-25 11:49:30 +0300

    fix dump script

--- apps/pgbench/create-pgbench-data-dump.bash
+++ apps/pgbench/create-pgbench-data-dump.bash
@@ -2,7 +2,7 @@
 
 set -eu
 
-docker rm -f postgres_bench_data || true
+docker rm -fv postgres_bench_data >/dev/null || true
 docker run --rm -d --name postgres_bench_data -e POSTGRES_PASSWORD=password postgres:14
 echo "Wait postgres init"
 sleep 10
@@ -10,4 +10,4 @@ docker exec -e PGPASSWORD=password postgres_bench_data pgbench -U postgres -h lo
 docker exec -e PGPASSWORD=password postgres_bench_data pg_dump -U postgres -h localhost -d postgres -a --column-inserts | \
   ./apps/pgbench/pg_dump_data_patch_format.sh
 
-docker rm -f postgres_bench_data || true
+docker rm -fv postgres_bench_data >/dev/null || true
