4263fe403c430c31b9e1e64a505c8a6b684159b0
Timofey Koolin <timofey.koolin@gmail.com>
2023-07-26 11:55:23 +0300

    Instruction for load data of pgbench

---
+++ apps/pgbench/README.md
@@ -0,0 +1,26 @@
+Instruction for upload pgbench data to YDB benchmark postgres layer.
+
+# Prepare data
+
+## Simple way
+Download already prepared data from [gist](https://gist.githubusercontent.com/rekby/95e0f0c2fd6ff595054c03c2c2ae776a/raw/pgbench.sql)
+
+```
+wget https://gist.githubusercontent.com/rekby/95e0f0c2fd6ff595054c03c2c2ae776a/raw/pgbench.sql -O tmp/pgbench.sql
+```
+
+## Generate new data with script
+
+Run create-pgbench-data-dump.sh. The script require installed docker.
+
+```bash
+cd <git-root>
+./apps/pgbench/create-pgbench-data-dump.bash > tmp/pgbench.sql
+```
+
+# Upload data to YDB.
+It can be uploaded with psql command:
+
+```bash
+cat pgbtmp/pgbench.sql | PGPASSWORD=password psql -h localhost -U root -d local -p 5432
+```
--- apps/pgbench/create-pgbench-data-dump.bash
+++ apps/pgbench/create-pgbench-data-dump.bash
@@ -2,12 +2,19 @@
 
 set -eu
 
-docker rm -fv postgres_bench_data >/dev/null || true
-docker run --rm -d --name postgres_bench_data -e POSTGRES_PASSWORD=password postgres:14
-echo "Wait postgres init"
+if [ ! -e .tests-root-folder ]; then
+  echo "Run from project root folder"
+  exit 1
+fi
+
+cat ./apps/pgbench/create-tables.sql
+
+docker rm -fv postgres_bench_data >/dev/null 2>&1 || true
+docker run --rm -d --name postgres_bench_data -e POSTGRES_PASSWORD=password postgres:14 >/dev/null 2>&1
+echo "Wait postgres init" 1>&2
 sleep 10
 docker exec -e PGPASSWORD=password postgres_bench_data pgbench -U postgres -h localhost -d postgres -i
-docker exec -e PGPASSWORD=password postgres_bench_data pg_dump -U postgres -h localhost -d postgres -a --column-inserts | \
+docker exec -e PGPASSWORD=password postgres_bench_data pg_dump -U postgres -h localhost -d postgres -a --column-inserts --rows-per-insert=1 | \
   ./apps/pgbench/pg_dump_data_patch_format.sh
 
-docker rm -fv postgres_bench_data >/dev/null || true
+docker rm -fv postgres_bench_data >/dev/null 2>&1 || true
