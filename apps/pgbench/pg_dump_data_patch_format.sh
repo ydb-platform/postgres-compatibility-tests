6cf9e1613fcbff541746e73b928da895116ffde0
Timofey Koolin <timofey.koolin@gmail.com>
2023-08-18 15:40:52 +0300

    disable remove set from pg_sump

--- apps/pgbench/pg_dump_data_patch_format.sh
+++ apps/pgbench/pg_dump_data_patch_format.sh
@@ -2,10 +2,9 @@
 
 set -eu
 
-sed -e 's/^SET .*//' | \
-  sed -e 's/^SELECT pg_catalog.set_config.*//' | \
-  sed -e 's/^INSERT INTO public./INSERT INTO /' | \
-  sed -e "s/ ')/ '::char)/" | \
-  sed -e "s/NULL)/NULL::char)/" | \
-  sed -e "s/', NULL::char/'::timestamp, NULL::char/" | \
-  cat
+sed -e 's/^SELECT pg_catalog.set_config.*//' | \
+sed -e 's/^INSERT INTO public./INSERT INTO /' | \
+sed -e "s/ ')/ '::char)/" | \
+sed -e "s/NULL)/NULL::char)/" | \
+sed -e "s/', NULL::char/'::timestamp, NULL::char/" | \
+cat
