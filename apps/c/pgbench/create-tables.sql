58915ff14c48ee0a4f1f1c41c776d01a746d771e
Timofey Koolin <timofey.koolin@gmail.com>
2023-09-25 15:47:27 +0300

    fix type of columns to pgbench

--- apps/c/pgbench/create-tables.sql
+++ apps/c/pgbench/create-tables.sql
@@ -1,5 +1,4 @@
-create table pgbench_history(tid int, bid int, aid int, delta int, mtime timestamp, filler char(22), primary key (mtime));
+create table pgbench_history(tid int, bid int, aid int, delta int, mtime timestamptz, filler char(22), primary key (mtime));
 create table pgbench_tellers(tid int not null,bid int,tbalance int,filler char(84), primary key (tid));
 create table pgbench_accounts(aid    int not null,bid int,abalance int,filler char(84), primary key (aid));
 create table pgbench_branches(bid int not null,bbalance int,filler char(88), primary key(bid));
-
