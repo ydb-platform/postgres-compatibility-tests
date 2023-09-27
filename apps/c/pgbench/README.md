e1764ec3e728c5b01f63eec31a8605f5fbbb7c8c
Timofey Koolin <timofey.koolin@gmail.com>
2023-09-27 11:08:50 +0300

    add dump with scale factor 9 to readme

--- apps/c/pgbench/README.md
+++ apps/c/pgbench/README.md
@@ -6,7 +6,10 @@ Instruction for upload pgbench data to YDB benchmark postgres layer.
 Download already prepared data from [gist](https://gist.githubusercontent.com/rekby/95e0f0c2fd6ff595054c03c2c2ae776a/raw/pgbench.sql)
 
 ```
-wget https://gist.githubusercontent.com/rekby/95e0f0c2fd6ff595054c03c2c2ae776a/raw/pgbench.sql -O tmp/pgbench.sql
+wget https://gist.githubusercontent.com/rekby/95e0f0c2fd6ff595054c03c2c2ae776a/raw/pgbench.sql -O tmp/pgbench.sql  # scale factor 1
+
+OR
+wget https://gist.githubusercontent.com/rekby/95e0f0c2fd6ff595054c03c2c2ae776a/raw/pgbench-9.sql -O tmp/pgbench.sql # scale factor 9, near 100MB file
 ```
 
 ## Generate new data with script
--- apps/c/pgbench/create-tables.sql
+++ apps/c/pgbench/create-tables.sql
@@ -1,4 +1,9 @@
-create table pgbench_history(tid int, bid int, aid int, delta int, mtime timestamptz, filler char(22), primary key (mtime));
-create table pgbench_tellers(tid int not null,bid int,tbalance int,filler char(84), primary key (tid));
-create table pgbench_accounts(aid    int not null,bid int,abalance int,filler char(84), primary key (aid));
-create table pgbench_branches(bid int not null,bbalance int,filler char(88), primary key(bid));
+DROP TABLE IF EXISTS pgbench_history;
+DROP TABLE IF EXISTS pgbench_tellers;
+DROP TABLE IF EXISTS pgbench_accounts;
+DROP TABLE IF EXISTS pgbench_branches;
+
+CREATE TABLE pgbench_history(tid int, bid int, aid int, delta int, mtime timestamptz, filler char(22), primary key (mtime));
+CREATE TABLE pgbench_tellers(tid int not null,bid int,tbalance int,filler char(84), primary key (tid));
+CREATE TABLE pgbench_accounts(aid    int not null,bid int,abalance int,filler char(84), primary key (aid));
+CREATE TABLE pgbench_branches(bid int not null,bbalance int,filler char(88), primary key(bid));
