fd629f5ea3493f9c2277d996717b19b33f646228
Timofey Koolin <timofey.koolin@gmail.com>
2023-10-27 18:55:57 +0300

    remove typing from dump patcher

--- apps/c/pgbench/pg_dump_data_patch_format.sh
+++ apps/c/pgbench/pg_dump_data_patch_format.sh
@@ -2,9 +2,5 @@
 
 set -eu
 
-sed -e 's/^SELECT pg_catalog.set_config.*//' | \
 sed -e 's/^INSERT INTO public./INSERT INTO /' | \
-sed -e "s/ ')/ '::char)/" | \
-sed -e "s/NULL)/NULL::char)/" | \
-sed -e "s/', NULL::char/'::timestamp, NULL::char/" | \
 cat
