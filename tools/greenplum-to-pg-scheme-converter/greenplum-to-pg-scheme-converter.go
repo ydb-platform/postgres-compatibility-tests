ebc60d0393ae64e27e4b321bc4fd88e91f6a7a04
Timofey Koolin <rekby@yandex-team.com>
2024-06-17 12:52:14 +0300

    converter work

--- tools/greenplum-to-pg-scheme-converter/greenplum-to-pg-scheme-converter.go
+++ tools/greenplum-to-pg-scheme-converter/greenplum-to-pg-scheme-converter.go
@@ -3,8 +3,11 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"io"
+	"log"
 	"os"
+	"strings"
 )
 
 type Config struct {
@@ -26,8 +29,11 @@ func main() {
 		Input:           bufio.NewScanner(reader),
 		Output:          bufio.NewWriter(writer),
 		CommentExcluded: cfg.CommentExcludedLines,
+
+		schemas: make(map[string]bool),
 	}
 
+	c.Convert()
 }
 
 func initConfig() Config {
@@ -70,18 +76,116 @@ type Converter struct {
 	Output          *bufio.Writer
 	CommentExcluded bool
 
-	state converterState
+	parseMode converterParseMode
+	schemas   map[string]bool
 }
 
-type converterState int
+type converterParseMode int
 
 const (
-	Empty converterState = iota
+	Empty converterParseMode = iota
 	CreateTable
 	CreateTableExcludeSuffix
 	CreateView
 )
 
 func (c *Converter) Convert() {
+	for c.Input.Scan() {
+		line := c.Input.Text()
+		result := c.processLine(line)
+		if result != "" && !strings.HasSuffix(result, "\n") {
+			result += "\n"
+		}
+		_, err := c.Output.WriteString(result)
+		if err != nil {
+			log.Fatalf("Failed to write line %q: %+v", result, err)
+		}
+	}
+}
 
+func (c *Converter) processLine(line string) string {
+	switch c.parseMode {
+	case Empty:
+		return c.processLineEmpty(line)
+	case CreateTable:
+		return c.processLineCreateTable(line)
+	case CreateTableExcludeSuffix:
+		return c.processLineCreateTableExcludeSuffix(line)
+	case CreateView:
+		return c.processLineCreateView(line)
+	default:
+		panic(fmt.Sprintf("Unexpected parseMode for converted process line: %v", c.parseMode))
+	}
+}
+
+func (c *Converter) processLineEmpty(line string) string {
+	switch {
+	case strings.HasPrefix(line, "CREATE SCHEMA "):
+
+		schemaName := strings.TrimPrefix(line, "CREATE SCHEMA ")
+		schemaName = strings.TrimSuffix(schemaName, ";")
+
+		return c.createSchema(schemaName)
+	case strings.HasPrefix(line, "CREATE TABLE "):
+		c.parseMode = CreateTable
+
+		schemaName := strings.TrimPrefix(line, "CREATE TABLE ")
+		schemaName, _, _ = strings.Cut(schemaName, ".")
+
+		if !c.schemas[schemaName] {
+			line = c.createSchema(schemaName) + line
+		}
+
+		return line
+	case strings.HasPrefix(line, "CREATE VIEW "):
+		c.parseMode = CreateView
+
+		schemaName := strings.TrimPrefix(line, "CREATE VIEW ")
+		schemaName, _, _ = strings.Cut(schemaName, ".")
+
+		if !c.schemas[schemaName] {
+			line = c.createSchema(schemaName) + line
+		}
+
+		return line
+	default:
+		return ""
+	}
+}
+
+func (c *Converter) processLineCreateTable(line string) string {
+	if line == ")" {
+		c.parseMode = CreateTableExcludeSuffix
+		return ")"
+	}
+
+	line, hasComma := strings.CutSuffix(line, ",")
+	line, _, _ = strings.Cut(line, " ENCODING ")
+	if hasComma {
+		line += ","
+	}
+	return line
+}
+
+func (c *Converter) processLineCreateTableExcludeSuffix(line string) string {
+	if strings.TrimSpace(line) == ";" {
+		c.parseMode = Empty
+		return ";\n\n"
+	}
+
+	return ""
+}
+
+func (c *Converter) processLineCreateView(line string) string {
+	if strings.HasSuffix(line, ";") {
+		c.parseMode = Empty
+		line += "\n\n"
+	}
+
+	return line
+}
+
+func (c *Converter) createSchema(schemaName string) string {
+	c.schemas[schemaName] = true
+	return "CREATE SCHEMA " + schemaName + ";\n"
 }
